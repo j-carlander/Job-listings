0fa857490875841a1dab6e2ff65b70d1e4036a29
J-Carlander <johannes.carlander@gmail.com>
2023-07-11 14:54:15 +0200

    prevent duplicates when adding filter

--- src/Components/FilterSection/FilterSection.jsx
+++ src/Components/FilterSection/FilterSection.jsx
@@ -5,16 +5,16 @@ export function FilterSection({role, level, languages, tools, handleFilter}){
     return (
         <section className="filter-section">
             {role && <FilterProperty property={role} handleFilter={() => {
-                handleFilter.setRoleFilter(prev => [...prev, role])
+                handleFilter.setRoleFilter(prev => !prev.includes(role) ? [...prev, role] : [...prev])
             }}/>}
             {level && <FilterProperty property={level} handleFilter={() => {
-                handleFilter.setLevelFilter(prev => [...prev, level])
+                handleFilter.setLevelFilter(prev => !prev.includes(level) ? [...prev, level] : [...prev])
             }} />}
             {languages.length >= 1 && languages.map(language => <FilterProperty property={language} key={language} handleFilter={() => {
-                handleFilter.setLanguageFilter(prev => [...prev, language])
+                handleFilter.setLanguageFilter(prev => !prev.includes(language) ? [...prev, language] : [...prev])
             }} />)}
             {tools.length >= 1 && tools.map(tool => <FilterProperty property={tool} key={tool} handleFilter={() => {
-                handleFilter.setToolFilter(prev => [...prev, tool])
+                handleFilter.setToolFilter(prev => !prev.includes(tool) ? [...prev, tool] : [...prev])
             }}/>)}
         </section>
     )
